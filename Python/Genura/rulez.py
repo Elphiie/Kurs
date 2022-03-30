9a1446c51cf05c7f7781fc2a97769064ddbf84cd
Elphiie <alven-erlend@Hotmail.com>
2022-03-30 17:02:36 +0200

    Start

---
+++ Python/Genura/rulez.py
@@ -0,0 +1,28 @@
+import imp
+from random import random
+from turtle import speed
+
+size = random(range(1, 100))
+
+energy_tot = random(range(5, 100))
+
+energy_use = random(range(1, 50)*size/2)
+
+unit_speed = random((range(0, 100)*energy_use)/1,5*size)
+
+speed
+class Movement:
+    def __init__(self, size, energy):
+        pass
+    
+
+class Energy:
+    def __init__(self) -> None:
+        pass
+
+
+class Env_avarenes:
+    pass
+
+class Mating:
+    pass

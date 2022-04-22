0628e97abf6bd8056b5fec65d564dc59a5109ae8
Elphiie <92294372+Elphiie@users.noreply.github.com>
2022-04-22 18:39:59 +0200

    Update visual.py

--- Python/Genura/visual.py
+++ Python/Genura/visual.py
@@ -1,8 +1,10 @@
 from ctypes import resize
+import math
 from random import randint
+from re import A
 from turtle import width
 import pyglet
-from pyglet import shapes, clock, sprite, image, resource
+from pyglet import shapes, clock, sprite, image, resource, app
 from pyglet.gl import *
 
 
@@ -13,77 +15,115 @@ main_batch = pyglet.graphics.Batch()
 
 batch = pyglet.graphics.Batch()
 
-
-dot_rng_pos_x = randint(0, game_window.width)
-dot_rng_pos_y = randint(0, game_window.height)
-
-rng_color = [randint(25, 255), randint(25, 255), randint(25, 255)]
-
-
-dot = shapes.Circle(dot_rng_pos_x, dot_rng_pos_y, 5, color=rng_color, batch = main_batch)
-
-def foods(num, batch = None):
-    new_food = []
-    for nums in range(0, num):
-        food_pos_x = randint(0, 800)
-        food_pos_y = randint(0, 600)
-        ball = pyglet.sprite.Sprite(ball_img, food_pos_x, food_pos_y, batch=batch)
-        new_food.append(ball)
-    return new_food
-
-def get_food(batch = None):
-    food_pos_x = randint(0, game_window.width-10)
-    food_pos_y = randint(0, game_window.height-10)
-    gen_food = shapes.Circle(food_pos_x, food_pos_y, 3, color=(255, 0, 0), batch = batch)
-    return gen_food
-
-all_food = [get_food() for i in range(50)]
-
-@game_window.event
-def rng_move(dt):
-    global x_i, y_i
-    x_i = randint(0, 10)
-    y_i = randint(0, 10)
-
-    if x_i == 1:
-        dot.x += 1
-            
-    elif x_i == 2:
-        dot.x -= 1
+#object setup
+#generate random x/y coordinates
+class Setup:
     
-    else:
-        dot.x += 0
+    dot_rng_pos_x = randint(0, game_window.width)
+    dot_rng_pos_y = randint(0, game_window.height)
+
+#random color generator that won't generate red as our food is red
+    rng_color = [randint(25, 50), randint(50, 255), randint(50, 255)]
+
+#the being
+    def life():
+        dot = shapes.Circle(Setup.dot_rng_pos_x, Setup.dot_rng_pos_y, 7, color=Setup.rng_color, batch = main_batch)
+        return dot
+
+#to store the food generated
+all_food = []
+#the dot
+dot = Setup.life()
+if dot.x > 799 or dot.y > 599:
+    dot.delete()
+#food generator
+class Food():
+
+    def get_food():
+        food_pos_x = randint(0, game_window.width-10)
+        food_pos_y = randint(0, game_window.height-10)
+        gen_food = shapes.Circle(food_pos_x, food_pos_y, 3, color=(255, 0, 0), batch = main_batch)
+        return gen_food
+
+global x_i, y_i
+class Movement:
+    global x_i, y_i
+    #just to check movement. rendunant code. remove or eddit
+    def rng_move(dt):
+    
+        x_i = randint(0, 10)
+        y_i = randint(0, 10)
+
+        if x_i == 1:
+            dot.x += 1
+                
+        elif x_i == 2:
+            dot.x -= 1
+        
+        else:
+            dot.x += 0
 
 
-    if y_i == 1:
-        dot.y += 1
+        if y_i == 1:
+            dot.y += 1
 
-    elif y_i == 2:
-        dot.y -= 1
+        elif y_i == 2:
+            dot.y -= 1
 
-    else:
-        dot.y += 0
+        else:
+            dot.y += 0
+
+    def hunt(dt):
+        target = None
+        target_d = float( 'inf' )
+
+        for e in list( all_food ):
+            d = math.dist( (dot.x, dot.y), (e.x, e.y) )
+
+            
+            if d > 0:
+                
+        
+        
+            
+            
 
 
+        
+
+        
+#just to check movement. rendunant code. remove or eddit
+
+#gettin everything to show on screen
 @game_window.event
 def on_draw():
     game_window.clear()
     main_batch.draw()
 
+#spawns food if the number of food is less than n
+def spawn_food(dt):
+    if len(all_food) < 50:
+        all_food.append(Food.get_food())
 
-@game_window.event
-def update(dt):
-    game_window.clear()
-    batch.draw()
+    else:
+        pass
+    
 
-clock.schedule_interval(update, 5)
+def respawn(dt):
+     if dot == None:
+        Setup.life()
+
+
 
 
 if __name__ == "__main__":
     # Start it up!
 
     # Update the game 120 times per second
-    clock.schedule_interval(rng_move, 1/120)
 
+    clock.schedule_interval(Movement.hunt, 1/130)
+    clock.schedule_interval(respawn, 1/130)
+    #spawns the food at random intervals
+    clock.schedule_interval(spawn_food, randint(0, 5))
     # Tell pyglet to do its thing
-    pyglet.app.run()
+    app.run()

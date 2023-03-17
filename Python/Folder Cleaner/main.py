9c14e23a6ba78f54dbd410775e3e8f97f6cbff33
Elphiie <92294372+Elphiie@users.noreply.github.com>
2023-03-18 00:15:04 +0100

    moved ai test to owb repository

--- Python/Folder Cleaner/main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("", topdown=False):
-    for dir in dirs:
-        if dir.startswith(dir):
-            filepath = os.path.join(root, dir)  
-            print (filepath)
